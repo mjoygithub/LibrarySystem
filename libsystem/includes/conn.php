1e3f62db6c6c798a33ebfd254a7768340cec260f
mjoygithub <167740709+mjoygithub@users.noreply.github.com>
2025-10-22 16:34:56 +0800

    Update conn.php

--- libsystem/includes/conn.php
+++ libsystem/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-// Render PostgreSQL connection string
+// PostgreSQL connection settings for Render
 $host = "dpg-d3s8k39r0fns73aag4sg-a.singapore-postgres.render.com";
 $port = "5432";
 $dbname = "libystem_db_6clw";
@@ -7,13 +7,17 @@ $user = "libystem_db";
 $password = "8NFTIb4Pc7SbFZ4Gbt8N4DqwQtXUWwDv";
 
 // Build connection string
-$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password options='--client_encoding=UTF8'";
 
 // Connect to PostgreSQL
 $conn = pg_connect($conn_string);
 
 // Check connection
 if (!$conn) {
+    // Use pg_last_error() for PostgreSQL-specific error
     die("Connection failed: " . pg_last_error());
 }
+
+// Optional: set error reporting for queries
+pg_query($conn, "SET CLIENT_ENCODING TO 'UTF8';");
 ?>
